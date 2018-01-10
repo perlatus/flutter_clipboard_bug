32c849268bce35bae92b96c5e9c4ee7148caefcf
perlatus <git@perlat.us>
2018-01-10 12:14:02 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  Function _paste(BuildContext ctx) {
+    return () async {
+      ClipboardData data = await Clipboard.getData(Clipboard.kTextPlain);
+      Scaffold.of(ctx).showSnackBar(new SnackBar(
+            content: new Text(
+              data != null
+                  ? 'Clipboard: "${data.text}"'
+                  : 'ClipboardData is null',
+            ),
+          ));
+    };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(home: new Scaffold(
+      floatingActionButton: new Builder(builder: (ctx) {
+        return new FloatingActionButton(
+          onPressed: _paste(ctx),
+          tooltip: "Paste clipboard",
+          child: new Icon(Icons.content_paste),
+        );
+      }),
+    ));
+  }
+}
